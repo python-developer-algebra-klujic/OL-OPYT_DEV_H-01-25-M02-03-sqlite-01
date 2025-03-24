056d063e9ad44959c6e536795dc3963b82d963a1
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-24 19:36:05 +0100

    Create project and task

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from repositories import (init_database,
-                          create_project)
+                          create_project,
+                          create_task)
 
 
 def main():
     init_database()
     create_project()
+
+    project_id = 1 # TODO Dohvatiti iz baze!!!
+    create_task(project_id)
     # Nastavak izvrsavanja programa
 
 
--- repositories/db_repositories.py
+++ repositories/db_repositories.py
@@ -39,6 +39,9 @@ def create_project():
         VALUES(?, ?, ?)
     '''
 
+    # TODO Provjeriti postoji li ovaj projekt u bazi?
+    # Ako postoji azuriraj ga, a ako ne postoji kreiraj ga
+
     try:
         # 1. KORAK Kreiranje konekcije
         with sqlite3.connect(DATABASE) as conn:
@@ -58,5 +61,32 @@ def create_project():
         print(f'Dogodila se greska {ex}.')
 
 
-def create_task():
-    pass
+def create_task(project_id: int):
+    insert_task_statement = '''
+        INSERT INTO tasks(name, priority, status_id,
+                            begin_date, end_date, project_id)
+        VALUES(?, ?, ?, ?, ?, ?)
+    '''
+
+    # TODO Provjeriti postoji li ovaj projekt u bazi?
+    # Ako postoji azuriraj ga, a ako ne postoji kreiraj ga
+
+    try:
+        # 1. KORAK Kreiranje konekcije
+        with sqlite3.connect(DATABASE) as conn:
+            # 2. KORAK Kreiranje Cursor objekta za rad s bazom (predstavlja nasu bazu)
+            cursor = conn.cursor()
+
+            # 3. KORAK Izvrsavanje SQL Query naredbi
+            cursor.execute(insert_task_statement,
+                           ('Task 1', 1, 1,
+                            '2025-01-31', '2025-02-10', project_id))
+
+            # Pokreni postupak snimanja promjena u bazi!!!
+            conn.commit()
+
+    except sqlite3.OperationalError as e:
+        print(f'Failed to open database: {e}')
+
+    except Exception as ex:
+        print(f'Dogodila se greska {ex}.')
